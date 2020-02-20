fa36cabadc611b83b9062623b1a0fdc70db9a204
laser <l@s3r.me>
2020-02-20 14:21:05 -0800

    add comments

--- policies/selfdeal/basic.go
+++ policies/selfdeal/basic.go
@@ -12,24 +12,43 @@ type Chain interface {
 	GetChainHead(ctx context.Context) (node.TipSetToken, abi.ChainEpoch, error)
 }
 
+// BasicPolicy is a SelfDealPolicy. It has two modes:
+//
+// Mode 1: The sector contains a non-zero quantity of client-provided pieces
+// Mode 2: The sector contains only self-deal pieces
+//
+// The BasicPolicy#Schedule method is given a slice of the pieces which the
+// miner intends to seal into the sector, and from that slice picks either the
+// first or second mode.
+//
+// If we're in Mode 1: The self-deal start and end-dates of the returned
+// DealSchedule will be set to the minimum and maximum start and end epoch
+// for pieces in the sector as long as at least one piece's deal contains both
+// start and end-dates which is in the future. Otherwise, fall back to Mode 2.
+//
+// If we're in Mode 2: The self-deal start date will be set to the current
+// epoch + provingDelay. The self-deal end date will be set to the current epoch
+// + provingDelay + duration.
 type BasicPolicy struct {
 	api Chain
 
 	// An estimate for the number of blocks between the current chain head and
-	// when a sector should have been proven. Used to compute the self-deal
-	// StartEpoch.
+	// when the sector should have been proven. Used to compute the self-deal
+	// start epoch.
 	provingDelay abi.ChainEpoch
 
-	// The number of epochs for which the self-dealing miner will receive power.
+	// The number of epochs for which the self-dealing miner will be required to
+	// honor the self-deal.
 	duration abi.ChainEpoch
 }
 
-// NewBasicPolicy produces a new fixed duration self-deal policy.
+// NewBasicPolicy produces a new BasicPolicy.
 func NewBasicPolicy(api Chain, delay abi.ChainEpoch, duration abi.ChainEpoch) BasicPolicy {
 	return BasicPolicy{api: api, provingDelay: delay, duration: duration}
 }
 
-// Schedule produces the deal terms for this fixed duration self-deal policy.
+// Schedule produces the deal schedule for the self-deals to reside in a sector
+// containing the provided pieces.
 func (p *BasicPolicy) Schedule(ctx context.Context, pieces ...node.PieceWithOptionalDealInfo) (node.DealSchedule, error) {
 	_, epoch, err := p.api.GetChainHead(ctx)
 	if err != nil {
