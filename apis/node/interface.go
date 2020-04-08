f0f62a4bf25e8bbcec76e20cb8c6b25c55712f5c
laser <l@s3r.me>
2020-04-08 16:03:17 -0700

    sealing2 -> fsm and other renaming

--- apis/node/interface.go
+++ apis/node/interface.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/filecoin-project/specs-actors/actors/abi"
 
-	sealing2 "github.com/filecoin-project/storage-fsm"
+	fsm "github.com/filecoin-project/storage-fsm"
 
 	"github.com/filecoin-project/go-address"
 )
@@ -13,19 +13,19 @@ import (
 type Interface interface {
 	// Sealing provides an interface to the node required by the finite state
 	// machine.
-	sealing2.SealingAPI
+	fsm.SealingAPI
 
 	// Events calls various callbacks when the chain progresses to (or rolls
 	// back to) an epoch.
-	sealing2.Events
+	fsm.Events
 
 	// GetMinerWorkerAddress produces the worker address associated with the
 	// provided miner address.
-	GetMinerWorkerAddress(ctx context.Context, maddr address.Address, tok sealing2.TipSetToken) (address.Address, error)
+	GetMinerWorkerAddress(ctx context.Context, maddr address.Address, tok fsm.TipSetToken) (address.Address, error)
 
 	// GetSealTicket produces a ticket from the chain to which the miner commits
 	// when they start encoding a sector.
-	GetSealTicket(ctx context.Context, tok sealing2.TipSetToken) (abi.SealRandomness, abi.ChainEpoch, error)
+	GetSealTicket(ctx context.Context, tok fsm.TipSetToken) (abi.SealRandomness, abi.ChainEpoch, error)
 
 	// WalletHas checks the wallet for the key associated with the provided
 	// address.
--- miner.go
+++ miner.go
@@ -13,34 +13,35 @@ import (
 	sectorstorage "github.com/filecoin-project/sector-storage"
 	"github.com/filecoin-project/sector-storage/ffiwrapper"
 	"github.com/filecoin-project/specs-actors/actors/abi"
-	sealing2 "github.com/filecoin-project/storage-fsm"
+	fsm "github.com/filecoin-project/storage-fsm"
 
 	"github.com/filecoin-project/go-storage-miner/apis/node"
 )
 
 var log = logging.Logger("storageminer")
 
-type Miner struct {
-	api                node.Interface                                 // this Miner's interface to the outside world
-	sealer             sectorstorage.SectorManager                    // a concrete implementation of sector-storage
-	minerAddressGetter func(context.Context) (address.Address, error) // used to acquire the miner's address, which can change over time
-	sealing            *sealing2.Sealing                              // finite-state machine for sectors
+type MinerAddressGetter = func(context.Context) (address.Address, error)
 
-	ds datastore.Batching       // responsible for persisting/loading state machine to/from disk
-	sc sealing2.SectorIDCounter // used to ensure that sectors are numbered uniquely for a given miner
-	vf ffiwrapper.Verifier      // an interface to proof verification
-	pp sealing2.PreCommitPolicy // used to set pre-commit expiry
+type Miner struct {
+	api node.Interface              // this Miner's interface to the outside world
+	dst datastore.Batching          // responsible for persisting/loading state machine to/from disk
+	fsm *fsm.Sealing                // finite-state machine for sectors
+	mag MinerAddressGetter          // used to acquire the miner's address, which can change over time
+	pcp fsm.PreCommitPolicy         // used to set pre-commit expiry
+	sid fsm.SectorIDCounter         // used to ensure that sectors are numbered uniquely for a given miner
+	mgr sectorstorage.SectorManager // a concrete implementation of sector-storage
+	ver ffiwrapper.Verifier         // an interface to proof verification
 }
 
-func NewMiner(api node.Interface, minerAddressGetter func(context.Context) (address.Address, error), ds datastore.Batching, sealer sectorstorage.SectorManager, sc sealing2.SectorIDCounter, vf ffiwrapper.Verifier, pp sealing2.PreCommitPolicy) *Miner {
+func NewMiner(api node.Interface, minerAddressGetter MinerAddressGetter, dst datastore.Batching, mgr sectorstorage.SectorManager, sid fsm.SectorIDCounter, ver ffiwrapper.Verifier, pcp fsm.PreCommitPolicy) *Miner {
 	return &Miner{
-		api:                api,
-		sealer:             sealer,
-		minerAddressGetter: minerAddressGetter,
-		ds:                 ds,
-		sc:                 sc,
-		vf:                 vf,
-		pp:                 pp,
+		api: api,
+		mgr: mgr,
+		mag: minerAddressGetter,
+		dst: dst,
+		sid: sid,
+		ver: ver,
+		pcp: pcp,
 	}
 }
 
@@ -56,7 +57,7 @@ func (m *Miner) Run(ctx context.Context) error {
 		return xerrors.Errorf("failed to get chain head during startup: %w", err)
 	}
 
-	maddr, err := m.minerAddressGetter(ctx)
+	maddr, err := m.mag(ctx)
 	if err != nil {
 		return xerrors.Errorf("failed to get miner address on startup: %w", err)
 	}
@@ -70,17 +71,17 @@ func (m *Miner) Run(ctx context.Context) error {
 		return xerrors.Errorf("miner preflight checks failed: %w", err)
 	}
 
-	m.sealing = sealing2.New(m.api, m.api, maddr, waddr, m.ds, m.sealer, m.sc, m.vf, m.api.GetSealTicket, m.pp)
+	m.fsm = fsm.New(m.api, m.api, maddr, waddr, m.dst, m.mgr, m.sid, m.ver, m.api.GetSealTicket, m.pcp)
 
 	log.Infof("starting up miner %s, worker addr %s", maddr, waddr)
 
-	return m.sealing.Run(ctx)
+	return m.fsm.Run(ctx)
 }
 
 // SealPiece writes the provided piece to a newly-created sector which it
 // immediately seals.
-func (m *Miner) SealPiece(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, sectorNum abi.SectorNumber, deal sealing2.DealInfo) error {
-	return m.sealing.SealPiece(ctx, size, r, sectorNum, deal)
+func (m *Miner) SealPiece(ctx context.Context, size abi.UnpaddedPieceSize, r io.Reader, sectorNum abi.SectorNumber, deal fsm.DealInfo) error {
+	return m.fsm.SealPiece(ctx, size, r, sectorNum, deal)
 }
 
 // Stop causes the miner to stop listening for sector state transitions. It is
@@ -88,7 +89,7 @@ func (m *Miner) SealPiece(ctx context.Context, size abi.UnpaddedPieceSize, r io.
 // behavior to call this method more than once. It is undefined behavior to call
 // this method concurrently with any other Miner method.
 func (m *Miner) Stop(ctx context.Context) error {
-	return m.sealing.Stop(ctx)
+	return m.fsm.Stop(ctx)
 }
 
 // AllocatePiece produces information about where a piece of a given size can
@@ -98,32 +99,32 @@ func (m *Miner) Stop(ctx context.Context) error {
 // means that we won't have the ability to move the piece around (i.e. do
 // intelligent bin packing) after allocating. -- @laser
 func (m *Miner) AllocatePiece(size abi.UnpaddedPieceSize) (sectorNum abi.SectorNumber, offset uint64, err error) {
-	return m.sealing.AllocatePiece(size)
+	return m.fsm.AllocatePiece(size)
 }
 
 // ListSectors lists all the sectors managed by this storage miner (sealed
 // or otherwise).
-func (m *Miner) ListSectors() ([]sealing2.SectorInfo, error) {
-	return m.sealing.ListSectors()
+func (m *Miner) ListSectors() ([]fsm.SectorInfo, error) {
+	return m.fsm.ListSectors()
 }
 
 // GetSectorInfo produces information about a sector managed by this storage
 // miner, or an error if the miner does not manage a sector with the
 // provided identity.
-func (m *Miner) GetSectorInfo(sectorNum abi.SectorNumber) (sealing2.SectorInfo, error) {
-	return m.sealing.GetSectorInfo(sectorNum)
+func (m *Miner) GetSectorInfo(sectorNum abi.SectorNumber) (fsm.SectorInfo, error) {
+	return m.fsm.GetSectorInfo(sectorNum)
 }
 
 // PledgeSector allocates a new sector, fills it with self-deal junk, and
 // seals that sector.
 func (m *Miner) PledgeSector(ctx context.Context) error {
 	// TODO: plumb context into the FSM
-	return m.sealing.PledgeSector()
+	return m.fsm.PledgeSector()
 }
 
 // ForceSectorState puts a sector with given ID into the given state.
-func (m *Miner) ForceSectorState(ctx context.Context, num abi.SectorNumber, state sealing2.SectorState) error {
-	return m.sealing.ForceSectorState(ctx, num, state)
+func (m *Miner) ForceSectorState(ctx context.Context, num abi.SectorNumber, state fsm.SectorState) error {
+	return m.fsm.ForceSectorState(ctx, num, state)
 }
 
 func (m *Miner) runPreflightChecks(ctx context.Context, waddr address.Address) error {
